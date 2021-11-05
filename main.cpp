3f2acb0617448b8821d644f866c587101051d8f9
DavidK1412 <68391937+DavidK1412@users.noreply.github.com>
2021-11-05 11:59:56 -0500

    Repaso funciones, tipos de datos, entraday salida con formats.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using namespace std; //Libreria que evita el std::
+
+float varTypes(int a, int b,float y){
+    float sum = a + b + y;
+    return sum;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n1, n2;
+    float n3, sum;
+    /*
+     * Scanning with scanf() function
+     * */
+    cout<<"Dos números enteros separados por un espacio: \n";
+    scanf("%d %d", &n1, &n2); //SCANF = First arg is the "codetype" of the declared vars and the another args are for the assignement
+    cout<<"Un decimal: \n";
+    scanf("%f", &n3);
+    sum = varTypes(n1, n2, n3);
+    /*
+     * Print with printf() function, isn't equal to print with cout word
+     * */
+    printf("La suma de sus valores ingresados es igual a : %5.2f", sum);
     return 0;
 }
