5c9815d7e67dee35573373d1aed233f3bcfcbb32
DavidK1412 <68391937+DavidK1412@users.noreply.github.com>
2021-11-05 12:27:58 -0500

    Basics.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std; //Libreria que evita el std::
 
@@ -7,9 +8,25 @@ float varTypes(int a, int b,float y){
     return sum;
 }
 
+void imprimirConCiclos(int x){
+    for(int i = 0; i <= x; i++){
+        printf("%d \n", i);
+    }
+    int i = 0;
+    do{
+        printf("Hola! \n");
+        i++;
+    }while (i <= 2);
+
+    while(i <= 10){
+        printf("Adios! %d\n", i);
+        i++;
+    }
+}
+
 int main() {
     int n1, n2;
-    float n3, sum;
+    float n3, sum, altura;
     /*
      * Scanning with scanf() function
      * */
@@ -18,9 +35,25 @@ int main() {
     cout<<"Un decimal: \n";
     scanf("%f", &n3);
     sum = varTypes(n1, n2, n3);
-    /*
+    /***
      * Print with printf() function, isn't equal to print with cout word
      * */
-    printf("La suma de sus valores ingresados es igual a : %5.2f", sum);
+    printf("La suma de sus valores ingresados es igual a : %5.2f \n", sum);
+    cout<<"Ingrese el número que quiere que se imprima desde 0 hasta x \n";
+    scanf("%d", &n1);
+    imprimirConCiclos(n1);
+
+    /***
+     *  CONDITIONS
+     * */
+    printf("Ingrese su altura (con punto flotante): \n");
+    scanf("%f", &altura);
+    if(altura <= 1.60){
+        printf("Es bajo!");
+    }else if(altura <= 1.78){
+        printf("Altura promedio en contexto Colombiano");
+    }else{
+        printf("Es alto!");
+    }
     return 0;
 }
